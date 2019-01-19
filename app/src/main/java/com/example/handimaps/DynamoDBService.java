98be24470fe19ff43eba51946b2298503978994e
Zachary Mitchell <zdmitche@calpoly.edu>
2019-01-19 10:30:31 -0800

    Added database service template

---
+++ app/src/main/java/com/example/handimaps/DynamoDBService.java
@@ -0,0 +1,25 @@
+import com.example.handimaps.DataService;
+import com.example.handimaps.Route;
+import com.example.handimaps.User;
+
+public class DynamoDBService implements DataService {
+    @Override
+    public Route SelectRoute(int routeId) {
+
+    }
+
+    @Override
+    public void InsertRoute(Route route) {
+
+    }
+
+    @Override
+    public User SelectUser(int userId) {
+        return null;
+    }
+
+    @Override
+    public void InsertUser(User user) {
+
+    }
+}
