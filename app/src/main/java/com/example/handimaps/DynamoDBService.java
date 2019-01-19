adc9e2c443a272aebcb69c56c038c7da1b42a5a9
Thomkaar <thomkaar@gmail.com>
2019-01-19 13:36:44 -0800

    started connection to DynamoDB

--- app/src/main/java/com/example/handimaps/DynamoDBService.java
+++ app/src/main/java/com/example/handimaps/DynamoDBService.java
@@ -2,15 +2,30 @@ package com.example.handimaps;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 
 public class DynamoDBService implements DataService {
+    private Table userTable;
+    private Table routeTable;
+
     public DynamoDBService() {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+        DynamoDB dynamoDB = new DynamoDB(client);
+        userTable = dynamoDB.getTable("User");
+        routeTable = dynamoDB.getTable("Route");
     }
 
     @Override
     public Route SelectRoute(int routeId) {
-        return null;
+        Item item = routeTable.getItem("route_id", routeId);
+        Route rtn = new Route();
+        rtn.setEnd_location(item.get("end_location").toString());
+        rtn.setIntermediate_loc(item.get("intermediate_loc").toString());
+        rtn.setStart_location(item.get("start_location").toString());
+        //rtn.setMade_by_user(item.get("made_by_user").());
+        return rtn;
     }
 
     @Override
--- app/src/main/java/com/example/handimaps/MainActivity.java
+++ app/src/main/java/com/example/handimaps/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+        DataService dataService = new DynamoDBService();
     }
 }
