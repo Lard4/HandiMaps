80159baffe9e82ca9d4e2f090035ed9fd6872cc5
Kevin Dixson <kdixson538@gmail.com>
2019-01-18 23:11:36 -0800

    Android Studio bringup

---
+++ app/src/main/java/com/example/handimaps/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.handimaps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
