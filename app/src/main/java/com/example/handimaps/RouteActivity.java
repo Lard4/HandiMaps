fa445d749d5dee8ba04624ac23e47dae67eb5eb1
Andrew Puleo <apuleo36@gmail.com>
2019-01-19 10:54:44 -0800

    add object

---
+++ app/src/main/java/com/example/handimaps/RouteActivity.java
@@ -0,0 +1,17 @@
+package com.example.handimaps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.
+
+public class RatingsActivity extends AppCompatActivity {
+    List<LatLng> coordinates = new ArrayList<>(); // Accumulating list of coordinates
+
+}
+
